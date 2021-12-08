c87feae7e3a4685a75980eaf5bc8b8f15cbf2e9a
Marcos Gabriel <jorgede1097@gmail.com>
2021-12-07 21:30:02 -0300

    Primeiro Commit

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.Nome = "Bob";
+            pessoa1.Idade = 22;
+
+            pessoa1.Apresentar();
+        }
+    }
+}
